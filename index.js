d1fe044bcf575cbb08fb76cdb9be5259266abfe9
LTS-gwanganlii <lts.gwangalli@gmail.com>
2025-02-11 20:34:16 +0900

    Update Instagram click handler to provide detailed user instructions and redirect to camera

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ google.addEventListener("click",()=>{
 })
 
 insta.addEventListener("click",()=>{
-    alert("스토리로 작성해주시고 매장을 태그 해 주세요!");
-    window.location.href = "https://www.instagram.com/lts.eh/";
+    alert("스토리로 작성해주시고 매장을 태그 해 주세요! \n 사진 1장 리뷰 2줄 부탁드립니다! \n 카메라가 자동으로 켜집니다");
+    window.location.href = "instagram://camera";
 })
